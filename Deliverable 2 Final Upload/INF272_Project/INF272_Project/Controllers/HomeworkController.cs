eb0bbd17103b822f347b13b2e513620ca126c053
Manesan <Manesanpillay28@gmail.com>
2019-10-15 20:49:21 +0200

    Screens
    Screens

---
+++ Deliverable 2 Final Upload/INF272_Project/INF272_Project/Controllers/HomeworkController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272_Project.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        // GET: Homework
+        public ActionResult UserHW()
+        {
+            return View();
+
+        }
+        public ActionResult UserHWResults()
+        {
+            return View();
+        }
+    }
+}
