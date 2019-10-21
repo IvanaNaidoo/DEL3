20adb1852d71fc4d10bc58e3f1f6639de19cd32c
Manesan <Manesanpillay28@gmail.com>
2019-10-21 22:22:15 +0200

    VMs

---
+++ New ViewModels/DonationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INF272_Project.ViewModels
+{
+    public class DonationViewModel
+    {
+        public string Description { get; set; }
+        public double Amount { get; set; }
+
+
+        public List<DonationViewModel> Donations { get; set; }
+
+
+        public DonationViewModel()
+        {
+            
+            Description = "";
+            Amount = 0; 
+        }
+        public DonationViewModel(string description, double amount)
+        {
+            Description = description;
+            Amount = amount;
+
+
+
+        }
+    }
+}
