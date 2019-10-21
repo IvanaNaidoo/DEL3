269ed6f3c6ddc46e358db49bc9d64776eb3713c3
Manesan <Manesanpillay28@gmail.com>
2019-10-22 00:25:46 +0200

    DEL3

---
+++ Del3/INF272_Project/INF272_Project/ViewModels/New/ReportViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INF272_Project.ViewModels
+{
+    public class ReportViewModel : HomeworkVM
+    {
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public ReportViewModel(string homeworkdescription, string subject, int level, string questions, int markachieved, string duedate, string description, DateTime date)
+        {
+            HomeworkDescription = homeworkdescription;
+            Subject = subject;
+            Level = level;
+            Questions = questions;
+            MarkAchieved = markachieved;
+            DueDate = duedate;
+            Description = description;
+            Date = date;
+
+
+        }
+
+    }
+}
---
+++ Del3/INF272_Project/INF272_Project/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INF272_Project.ViewModels
+{
+    public class SignUpViewModel : LearnerViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public SignUpViewModel(string Name, string Surname, string Email, string Number, string ParentName, string ParentSurname, string ParentEmail, string username, string password) : base(Name, Surname, Email, Number, ParentName, ParentSurname, ParentEmail)
+        {
+            Username = username;
+            Password = password;
+
+        }
+    }
+}
