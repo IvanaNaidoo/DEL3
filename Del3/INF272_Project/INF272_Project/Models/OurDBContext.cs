422127f29c819098727e2284199020b691ab5a8e
Tiara Kiashka Surajbali <kiashka28@gmail.com>
2019-10-22 01:26:10 +0200

    Update

---
+++ Del3/INF272_Project/INF272_Project/Models/OurDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace INF272_Project.Models
+{
+    public class OurDBContext : DbContext
+    {
+        public DbSet<UserAccount> UserAccount { get; set; }
+
+
+    }
+}
